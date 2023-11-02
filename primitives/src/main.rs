48605f649e1370e53b7cb5d4aff3163cc0475e38
Roman Makeev <makeevrserg@gmail.com>
2023-11-02 20:46:37 +0300

    feat: user_input

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -5,5 +5,5 @@ fn main() {
 fn sum_numbers() {
     let num = 10;
     let num_sum = 10 + 1;
-    println!("Hello, world! {num} plus one is {num_sum}!");
+    println!("Hello, world! {num} plus one is {num_sum}!","");
 }
