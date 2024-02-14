ed6ff13ae27e0247f07065249dd1ba3854a31b6e
Roman Makeev <makeevrserg@gmail.com>
2024-02-14 19:27:43 +0300

    improve guessing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
+use colored::*;
 
 fn main() {
     println!("Guess the number!");
@@ -11,17 +12,28 @@ fn main() {
 
     println!("Please input your guess.");
     let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{}","Invalid value!".red());
+                continue;
+            }
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}","Too small!".red()),
+            Ordering::Greater => println!("{}","Too big!".red()),
+            Ordering::Equal => {
+                println!("{}","You win!".green());
+                break;
+            }
+        }
     }
 }
